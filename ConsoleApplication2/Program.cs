4ec9c13cba31f5da217be560950b77b654b1ca18
APPBRIDGE\xlu <xlu@appbridge.io>
2017-02-22 17:20:27 -0800

    Updated some utility functions for the same of testing

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,8 +1,12 @@
 ﻿using LoveSeat;
 using MyCouch;
+using MyCouch.Net;
+using MyCouch.Requests;
+using MyCouch.Responses;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,18 +28,68 @@ namespace ConsoleApplication2
             //// get document by ID
             //Document myDoc = db.GetDocument("68e56facc00392d9a35dde46860019ad");
 
-            string username = "root";
-            string password = "7";
-            string database = "t1";
 
-            Post2CouchDB(username, password, database);
+            // MyCouch
+            string username = "root";
+            string password = "111111";
+            string database = "t1";
+            string id = "whatever";
+
+            string filepath = @"C:\Users\xlu.APPBRIDGE\Desktop\db_comp.pdf";
+            string attachmentName = "test1";
+            string documentId = "whatever";
+
+            //var byteArr = TurnFile2Byte(filepath);
+            //PostAttachment(byteArr, username, password, database, id);
+
+            var byteArr = GetAttachment(documentId, attachmentName, username, password, database);
+            var byteArrRes = byteArr.Result;
+
+            SaveArr2File(byteArrRes);
+
             Console.ReadKey();
+
         }
 
-        public static async void Post2CouchDB(string username, string password, string database)
+        public static async Task<byte[]> GetAttachment(string documentId, string attachmentName, string username, string password, string database)
         {
-            string connString = string.Format("http://{0}:{1}@localhost:5984", username, password);
-            Console.WriteLine(connString);
+            const string ipaddress = "localhost";
+            const string port = "5984";
+
+            string connString = string.Format("http://{0}:{1}@{2}:{3}", username, password, ipaddress, port);
+
+            var client = new MyCouchClient(connString, database);
+
+            AttachmentResponse response = await client.Attachments.GetAsync(documentId, attachmentName);
+
+            return response.Content;
+        }
+
+        public static async void PostAttachment(byte[] byteArr, string username, string password, string database, string id)
+        {
+            const string ipaddress = "localhost";
+            const string port = "5984";
+
+            string connString = string.Format("http://{0}:{1}@{2}:{3}", username, password, ipaddress, port);
+
+            var request = new PutAttachmentRequest(id, "test1", HttpContentTypes.Text, byteArr);
+
+            var client = new MyCouchClient(connString, database);
+
+            var response = await client.Attachments.PutAsync(request);
+
+            Console.WriteLine(response.IsSuccess);
+
+            Console.WriteLine(response.Reason);
+        }
+
+        public static async Task<bool> Post2CouchDB(string username, string password, string database, string id)
+        {
+            const string ipaddress = "localhost";
+            const string port = "5984";
+
+            string connString = string.Format("http://{0}:{1}@{3}:{4}", username, password, ipaddress, port);
+            //Console.WriteLine(connString);
 
             using (var client = new MyCouchClient(connString, database))
             {
@@ -49,25 +103,48 @@ namespace ConsoleApplication2
                 s.Name = "for the first time ever to get this work";
                 s.Gpa = 80;
                 s.Description = "this is gonna be anwesome description that i post to the database  fucking awesome";
-                s.Other = " wow this whole newtonsoft thign is actually prtty alwesome";
+                s.Other = "wow this whole newtonsoft thign is actually prtty alwesome";
                 s.HerBook = b;
 
                 string jsonString = JsonConvert.SerializeObject(s);
 
-                try
+                DocumentHeaderResponse response = await client.Documents.PutAsync(id, jsonString);
+
+                if (!response.IsSuccess)
                 {
-                    await client.Documents.PutAsync("68e56facc00392d9a35dde468600915f", "1-96254bdaf067b68dde927a3f386e9a55", jsonString);
+                    return false;
                 }
-                catch (Exception)
-                {
-                    throw;
-                }
+
+                return true;
             }
         }
 
 
+        public static void SaveArr2File(byte[] byteArr)
+        {
+            if (byteArr == null)
+            {
+                return;
+            }
+
+            File.WriteAllBytes(@"C:\Users\xlu.APPBRIDGE\Desktop\shit.pdf", byteArr);
+        }
+
+        public static byte[] TurnFile2Byte(string filepath)
+        {
+            if (string.IsNullOrEmpty(filepath))
+            {
+                return null;
+            }
+
+            return File.ReadAllBytes(filepath);
+        }
+
 
     }
+
+
+
 
 
     public class Student
@@ -76,7 +153,6 @@ namespace ConsoleApplication2
         public double Gpa { get; set; }
         public string Description { get; set; }
         public string Other { get; set; }
-
         public Book HerBook { get; set; }
     }
 
@@ -86,7 +162,6 @@ namespace ConsoleApplication2
         public string Author { get; set; }
         public string Type { get; set; }
         public int Page { get; set; }
-
     }
 
 }
